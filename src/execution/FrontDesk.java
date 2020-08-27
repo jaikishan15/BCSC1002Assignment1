0c0b9d12c228cd7a9b8fce883c9c425f58cda134
jaikishan15 <jai.kishan_cs19@gla.ac.in>
2020-08-27 19:34:47 +0530

    add Scanner class for studentInput

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,14 +6,20 @@
  * */
 package execution;
 
+import definitions.Student;
+
+import java.util.Scanner;
+
 public class FrontDesk {
     private static final int ISSUE_A_NEW_BOOK_FOR_ME = 1;
     private static final int RETURN_PREVIOUSLY_ISSUES_BOOK_FOR_ME = 2;
     private static final int SHOW_ME_ALL_MY_ISSUES_BOOKS = 3;
     private static final int EXIT = 4;
 
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int studentInput;
+        Student myDetails = new Student();
     }
 }
 

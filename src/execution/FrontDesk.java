e303f292b7f02c1cd37c416b972fa8b7b1db2fe6
jaikishan15 <jai.kishan_cs19@gla.ac.in>
2020-08-27 23:51:08 +0530

    add new

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,39 +6,8 @@
  * */
 package execution;
 
-import definitions.Student;
-
-import java.util.Scanner;
-
 public class FrontDesk {
-    private static final int ISSUE_A_NEW_BOOK_FOR_ME = 1;
-    private static final int RETURN_PREVIOUSLY_ISSUES_BOOK_FOR_ME = 2;
-    private static final int SHOW_ME_ALL_MY_ISSUES_BOOKS = 3;
-    private static final int EXIT = 4;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int studentInput;
-        Student myStudent = new Student();
-        do {
-            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-\n");
-            System.out.println("how may can i help you today");
-            System.out.println("1.issue a new book for me");
-            System.out.println("2.Return previously issues book for me");
-            System.out.println("3.show me all my issues books");
-            System.out.println("4.Exit");
-            System.out.println("Please enter your choice from 1..6");
-            studentInput = scanner.nextInt();
-            switch (studentInput) {
-                case ISSUE_A_NEW_BOOK_FOR_ME:
-                    System.out.println("enter the name of the book you want");
-                    scanner.nextLine();
-                    String nameOfTheBook = scanner.nextLine();
-                    myStudent.issueNewBook(nameOfTheBook);
-
-            }
-        }
-    }
 }
 
 
